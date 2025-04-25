760cae886d8bc80297fa3ef61c1a2e05912c0fa8
isabelsotomayor <isabel.sotomayor.c@gmail.com>
2025-04-25 16:25:14 +0000

    carta

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+  const palos = ['spade', 'club', 'heart', 'diamond'];
+  const simbolos = ['♠', '♣', '♥', '♦'];
+  const numeros = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
+  const paloIndex = Math.floor(Math.random() * palos.length);
+  const numeroIndex = Math.floor(Math.random() * numeros.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const palo = palos[paloIndex];
+  const simbolo = simbolos[paloIndex];
+  const numero = numeros[numeroIndex];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const carta = document.getElementById('card');
+  carta.classList.add(palo);
+  carta.querySelector('.top-suit').textContent = simbolo;
+  carta.querySelector('.number').textContent = numero;
+  carta.querySelector('.bottom-suit').textContent = simbolo;
 };
